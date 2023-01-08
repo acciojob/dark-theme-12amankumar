6c384df1879d7b8e7d7a59293d5548fa94701b42
12amankumar <117712947+12amankumar@users.noreply.github.com>
2023-01-08 11:55:43 +0530

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your code here
 function myFunction() {
     var element = document.getElementById("app");
-    element.className="night";
+    element.className="day";
     var button= document.getElementById("swap");
-    button.className="button_night";
+    button.className="button_day";
  }
